f8da583f14938cd4387cdcae8120b8a1b4dc396a
Ian Ibbotson <ianibbo@googlemail.com>
2012-03-24 16:50:04 +0000

    Added person org

---
+++ vfis/grails-app/domain/com/k_int/vfis/PersonOrg.groovy
@@ -0,0 +1,7 @@
+package com.k_int.vfis
+
+class PersonOrg {
+
+    static constraints = {
+    }
+}
---
+++ vfis/test/unit/com/k_int/vfis/PersonOrgTests.groovy
@@ -0,0 +1,17 @@
+package com.k_int.vfis
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(PersonOrg)
+class PersonOrgTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
