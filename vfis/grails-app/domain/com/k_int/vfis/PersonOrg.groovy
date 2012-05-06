8b7c2a9c65d341c5b06c4d7713f12b5e42f8e32e
Ian Ibbotson <ianibbo@googlemail.com>
2012-05-06 12:51:32 +0100

    replaced org and person org

---
+++ vfis/grails-app/domain/com/k_int/vfis/PersonOrg.groovy
@@ -0,0 +1,18 @@
+package com.k_int.vfis
+
+class PersonOrg {
+
+    Long dateRequested
+    Long dateActioned
+    int status  // 0=Pending, 1=Approved, 2=Rejected
+    String role
+    com.k_int.vfis.auth.VfisPerson actionedBy
+
+    static belongsTo = [org: com.k_int.vfis.Organisation, 
+                        person:com.k_int.vfis.auth.VfisPerson]
+  
+    static constraints = {
+      dateActioned(nullable:true)
+      actionedBy(nullable:true)
+    }
+}
