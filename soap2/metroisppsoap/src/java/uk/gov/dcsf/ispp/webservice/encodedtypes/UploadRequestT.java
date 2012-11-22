4afa95ec5514ab0199ec5bd6ad12b901fc641a7b
Ian Ibbotson <ianibbo@googlemail.com>
2012-11-22 17:10:29 +0000

    Generated stubs using metro

---
+++ soap2/metroisppsoap/src/java/uk/gov/dcsf/ispp/webservice/encodedtypes/UploadRequestT.java
@@ -0,0 +1,116 @@
+
+package uk.gov.dcsf.ispp.webservice.encodedtypes;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for uploadRequest_t complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="uploadRequest_t">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="doc" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="authoritative" type="{http://www.w3.org/2001/XMLSchema}boolean" minOccurs="0"/>
+ *         &lt;element name="owner" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "uploadRequest_t", propOrder = {
+    "doc",
+    "authoritative",
+    "owner"
+})
+public class UploadRequestT {
+
+    @XmlElement(required = true)
+    protected String doc;
+    protected Boolean authoritative;
+    protected String owner;
+
+    /**
+     * Gets the value of the doc property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDoc() {
+        return doc;
+    }
+
+    /**
+     * Sets the value of the doc property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDoc(String value) {
+        this.doc = value;
+    }
+
+    /**
+     * Gets the value of the authoritative property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Boolean }
+     *     
+     */
+    public Boolean isAuthoritative() {
+        return authoritative;
+    }
+
+    /**
+     * Sets the value of the authoritative property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Boolean }
+     *     
+     */
+    public void setAuthoritative(Boolean value) {
+        this.authoritative = value;
+    }
+
+    /**
+     * Gets the value of the owner property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getOwner() {
+        return owner;
+    }
+
+    /**
+     * Sets the value of the owner property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setOwner(String value) {
+        this.owner = value;
+    }
+
+}
---
+++ soap2/metroisppsoap/src/java/uk/gov/dcsf/ispp/webservice/encodedtypes/UploadResponseT.java
@@ -0,0 +1,116 @@
+
+package uk.gov.dcsf.ispp.webservice.encodedtypes;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for uploadResponse_t complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="uploadResponse_t">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="status" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="addinfo" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="location" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "uploadResponse_t", propOrder = {
+    "status",
+    "addinfo",
+    "location"
+})
+public class UploadResponseT {
+
+    @XmlElement(required = true)
+    protected String status;
+    protected String addinfo;
+    protected String location;
+
+    /**
+     * Gets the value of the status property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * Sets the value of the status property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setStatus(String value) {
+        this.status = value;
+    }
+
+    /**
+     * Gets the value of the addinfo property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAddinfo() {
+        return addinfo;
+    }
+
+    /**
+     * Sets the value of the addinfo property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAddinfo(String value) {
+        this.addinfo = value;
+    }
+
+    /**
+     * Gets the value of the location property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * Sets the value of the location property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLocation(String value) {
+        this.location = value;
+    }
+
+}
