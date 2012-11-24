5f4962b718c28b4d048afffba1d1d940b0d42a0f
Ian Ibbotson <ian.ibbotson@k-int.com>
2012-11-24 18:23:28 +0000

    Added checksum

--- soap2/cxfisppsoap/grails-app/services/cxfisppsoap/DepositService.groovy
+++ soap2/cxfisppsoap/grails-app/services/cxfisppsoap/DepositService.groovy
@@ -1,8 +1,13 @@
 package cxfisppsoap
 
+import java.security.MessageDigest
+
 class DepositService {
 
   def upload(file,authoritative,owner,user) {
+
+    def result = [:]
+
     log.debug("upload: file len=${file.length()}");
     log.debug("        ${authoritative}");
     log.debug("        ${owner}");
@@ -10,12 +15,30 @@ class DepositService {
 
     try {
       def xs=new net.sf.json.xml.XMLSerializer();
-      xs.setSkipNamespaces( true );
-      xs.setTrimSpaces( true );
+      xs.setSkipNamespaces( false );
+      xs.setTrimSpaces( false );
       xs.setRemoveNamespacePrefixFromElements( true );
-      def result = xs.read(file)
-      result.'$schema' = "http://purl.org/jsonschema/ecd";
-      log.debug("Converted ${result}"); // Result of conversion: ${result}")
+
+      result.json = xs.read(file)
+
+      result.json.cksum = chksum(file);
+
+      if ( result['@schemaLocation']?.startsWith('http://dcsf.gov.uk/XMLSchema/Childcare') ) {
+        log.debug("ECD Record");
+        result.json.'$schema' = "http://purl.org/jsonschema/ecd";
+      }
+      else {
+        if ( result['@schemaLocation']?.startsWith('http://dcsf.gov.uk/XMLSchema/ServiceDirectory') ) {
+          log.debug("FSD Record");
+          result.json.'$schema' = "http://purl.org/jsonschema/fsd";
+        }
+        else {
+          log.debug("Unknown type");
+        }
+      }
+
+      log.debug("Converted ${result.json}");
+
     }
     catch ( Exception e ) {
       log.error("error",e);
@@ -23,5 +46,20 @@ class DepositService {
     finally {
       log.debug("upload service completed");
     }
+  }
+
+  def chksum(file) {
+    MessageDigest md5_digest = MessageDigest.getInstance("MD5");
+    InputStream md5_is = new java.io.ByteArrayInputStream(file.getBytes());
+    byte[] md5_buffer = new byte[8192];
+    int md5_read = 0;
+    while( (md5_read = md5_is.read(md5_buffer)) >= 0) {
+      md5_digest.update(md5_buffer, 0, md5_read);
+    }
+    md5_is.close();
+    byte[] md5sum = md5_digest.digest();
+    String md5sumHex = new BigInteger(1, md5sum).toString(16);
+
+    md5sumHex
   }
 }
