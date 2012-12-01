90f564e06634a9b02cc1fe0a87853bcc2d0f3fa9
ibbo <ibbo@crick.(none)>
2012-12-01 11:22:03 +0000

    Wire in api rest deposit service - now calls common service endpoint

--- api/grails-app/controllers/cxfisppsoap/RestController.groovy
+++ api/grails-app/controllers/cxfisppsoap/RestController.groovy
@@ -4,16 +4,34 @@ package cxfisppsoap
 import grails.converters.*
 import grails.plugins.springsecurity.Secured
 import groovy.xml.MarkupBuilder
+import grails.converters.*
+import java.security.MessageDigest
+
 
 class RestController {
 
-  @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
+  def springSecurityService
+  def depositService
+
+  // Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
   def index() { 
   }
 
-  @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
+  // Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
   def deposit() {
     log.debug("Deposit");
-  }
+    if ( request.method == "POST" ) {
+      def user = springSecurityService.currentUser 
+      log.debug("POST: User: ${user}")
+      def provider = params.owner;
+      def file = request.getFile("upload")
 
+      def record = new String(file.getBytes())
+      depositService.upload(record,true,provider,user.username);
+      log.debug("rest deposit post done");
+    }
+
+    log.debug("rest deposit done");
+
+  }
 }
--- api/grails-app/views/rest/deposit.gsp
+++ api/grails-app/views/rest/deposit.gsp
@@ -3,5 +3,10 @@
 </head>
 <body>
 This is the localchatter deposit rest endpoint
+<form method="POST" enctype="multipart/form-data">
+  File: <input type="file" name="upload" label="File"/><br/>
+  On Behalf Of: <input type="text" name="owner" label="Owner"/><br/>
+  <input type="submit"/>
+<form>
 </body>
 </html>
