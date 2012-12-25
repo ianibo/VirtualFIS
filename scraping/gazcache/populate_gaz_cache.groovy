4e6fd100c28127a58defe7cd78c94af407e2c51a
Ian Ibbotson <ian.ibbotson@k-int.com>
2012-12-25 09:37:01 +0000

    Progress on gazcache

--- scraping/gazcache/populate_gaz_cache.groovy
+++ scraping/gazcache/populate_gaz_cache.groovy
@@ -2,10 +2,17 @@
 
 @Grapes([
   @Grab(group='com.gmongo', module='gmongo', version='0.9.2'),
-  @Grab( 'org.ccil.cowan.tagsoup:tagsoup:1.2.1' )
+  @Grab( 'org.ccil.cowan.tagsoup:tagsoup:1.2.1' ),
+  @Grab(group='org.apache.httpcomponents', module='httpmime', version='4.1.2'),
+  @Grab(group='org.apache.httpcomponents', module='httpclient', version='4.0'),
+  @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.0')
+
 ])
 
 import org.ccil.cowan.tagsoup.Parser
+import static groovyx.net.http.Method.*
+import groovyx.net.http.RESTClient
+import groovyx.net.http.*
 
 
 println "Open mongo";
@@ -50,6 +57,7 @@ def go(crawl_db, gazcache_db) {
         gaz_cache_hits++;
       }
       else {
+        geocode(postcode, gazcache_db);
         geocodes++;
       }
     }
@@ -57,3 +65,61 @@ def go(crawl_db, gazcache_db) {
     println("processed: ${records_processed}, geocodes: ${geocodes}, cache_hits: ${gaz_cache_hits}");
   }
 }
+
+def reverseGeocode(lat,lng) {
+
+  def searches = ["administrative_area_level_2","postal_code_prefix","route","locality"]
+  def geoLocation = ['county','postcode','street','locality']
+  def result =[:]
+  def http = new HTTPBuilder("http://maps.googleapis.com");
+  http.request(Method.valueOf("GET"), JSON) {
+    uri.path = '/maps/api/geocode/json'
+    uri.query = [ 'latlng' : "$lat,$lng", 'sensor' : 'false' ]
+    response.success = {resp, json ->
+      json.results[0].address_components.each { ac ->
+        def i = 0
+        while (i<4){
+          if ( ac.types.contains(searches[i]) ) {
+            result[geoLocation[i]]="${ac.long_name}"
+          }
+          i++
+        }
+      }
+    }
+  }
+  result
+}
+
+
+def geocode(postcode, gazcache_db) {
+
+  def searches = ["administrative_area_level_2","postal_code_prefix","route","locality"]
+  def geoLocation = ['county','postcode','street','locality']
+  def result =[:]
+  def http = new HTTPBuilder("http://maps.googleapis.com");
+  http.request(Method.valueOf("GET"), ContentType.JSON) {
+    uri.path = '/maps/api/geocode/json'
+    uri.query = [ 'address' : "$postcode", 'sensor' : 'false' ]
+    response.success = {resp, json ->
+
+      def cache_entry = [ address:postcode,
+                          response:json,
+                          lastSeen: System.currentTimeMillis(),
+                          created: System.currentTimeMillis() ]
+
+      gazcache_db.save(cache_entry);
+
+      // json.results[0].address_components.each { ac ->
+      //   def i = 0
+      //   while (i<4){
+      //     if ( ac.types.contains(searches[i]) ) {
+      //       result[geoLocation[i]]="${ac.long_name}"
+      //     }
+      //     i++
+      //   }
+      // }
+    }
+  }
+  result
+}
+
