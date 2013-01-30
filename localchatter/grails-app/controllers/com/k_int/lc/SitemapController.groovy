ca2c22a834f67dd48cf19edabb22af3dcf382cfe
Ian Ibbotson <ianibbo@gmail.com>
2013-01-30 18:03:37 +0000

    more work on sitefile

--- localchatter/grails-app/controllers/com/k_int/lc/SitemapController.groovy
+++ localchatter/grails-app/controllers/com/k_int/lc/SitemapController.groovy
@@ -1,6 +1,7 @@
 package com.k_int.lc
 
 import grails.converters.*
+import groovy.xml.MarkupBuilder
 
 class SitemapController {
 
@@ -10,8 +11,11 @@ class SitemapController {
 
     log.debug("SitemapController::index");
 
-     org.elasticsearch.groovy.node.GNode esnode = elasticSearchService.getESNode()
-     org.elasticsearch.groovy.client.GClient esclient = esnode.getClient()
+    org.elasticsearch.groovy.node.GNode esnode = elasticSearchService.getESNode()
+    org.elasticsearch.groovy.client.GClient esclient = esnode.getClient()
+
+    def formatter = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    def default_date = formatter.format(new Date(System.currentTimeMillis()))
 
     def result = [:]
     result.providers = []
@@ -43,7 +47,7 @@ class SitemapController {
         search.response.facets.facets.each { facet ->
           log.debug("Processing: ${facet}");
           facet.value.entries.each { fe ->
-            result.providers.add(fe.term)
+            result.providers.add([name:fe.term,count:fe.count,lastModified:getLastModified(fe.term,esclient)])
           }
         }
       }
@@ -56,6 +60,51 @@ class SitemapController {
       e.printStackTrace()
     }
 
-    render result as JSON
+    def writer = new StringWriter()
+    def xml = new MarkupBuilder(writer)
+
+    xml.sitemapindex('xmlns:xsd':'http://www.w3.org/2001/XMLSchema',
+                     'xmlns:xsi':'http://www.w3.org/2001/XMLSchema-instance',
+                     'xsi:schemaLocation':'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd',
+                     'xmlns':'http://www.sitemaps.org/schemas/sitemap/0.9',
+                     'targetNamespace':'http://www.sitemaps.org/schemas/sitemap/0.9') {
+      result.providers.each { prov ->
+        sitemap() {
+          loc("${grailsApplication.config.frontend}/sitemap/provider/${prov.name}")
+          lastmod( prov.lastModified != null ? prov.lastModified : default_date );
+          mkp.comment("Doc count for this authority: ${prov.count}")
+        }
+      }
+    }
+
+    println "Render...siteindex"
+
+    render(contentType:'application/xml', text: writer.toString())
+  }
+
+  def getLastModified(provid,esclient) {
+    log.debug("YAY")
+    def formatter = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    def default_date = formatter.format(new Date(System.currentTimeMillis()))
+
+    def search = esclient.search{
+      indices "localchatter"
+      types "resource"
+      source {
+        query {
+          query_string (query: "provider:${provid}")
+        }
+        sort = [
+                  'lastModified' : [
+                    'order' : 'asc',
+                  ]
+        ]
+      }
+    }
+
+    default_date
+  }
+
+  def provider() {
   }
 }
