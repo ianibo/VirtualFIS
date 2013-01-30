9ab31c7a360f97f416957dcc56ea4ecb06cc9902
Ian Ibbotson <ianibbo@gmail.com>
2013-01-30 17:40:07 +0000

    sitemap controller gets all provider facet values

--- localchatter/grails-app/controllers/com/k_int/lc/SitemapController.groovy
+++ localchatter/grails-app/controllers/com/k_int/lc/SitemapController.groovy
@@ -1,6 +1,61 @@
 package com.k_int.lc
 
+import grails.converters.*
+
 class SitemapController {
 
-    def index() { }
+  def elasticSearchService
+
+  def index() { 
+
+    log.debug("SitemapController::index");
+
+     org.elasticsearch.groovy.node.GNode esnode = elasticSearchService.getESNode()
+     org.elasticsearch.groovy.client.GClient esclient = esnode.getClient()
+
+    def result = [:]
+    result.providers = []
+    // query_string (query: '*:*')
+    try {
+      log.debug("Search....");
+      def search = esclient.search{
+        indices "localchatter"
+        types "resource"
+        source {
+          query {
+            match_all()
+          }
+          facets {
+            provider {
+              terms {
+                field = 'provider'
+                all_terms = true
+              }
+            }
+          }
+        }
+      }
+
+      log.debug("Search response: ${search}");
+      
+      if ( search.response.facets != null ) {
+        log.debug("Got facets");
+        search.response.facets.facets.each { facet ->
+          log.debug("Processing: ${facet}");
+          facet.value.entries.each { fe ->
+            result.providers.add(fe.term)
+          }
+        }
+      }
+      else {
+        log.debug("No facets");
+      }
+
+    }
+    catch ( Exception e ) {
+      e.printStackTrace()
+    }
+
+    render result as JSON
+  }
 }
