004d8fb2b7f5d171e4ecf0fa967b66135fe7577f
Ian Ibbotson <ianibbo@gmail.com>
2013-02-22 19:47:42 +0000

    Added clear2 script

---
+++ scripts/clear2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# A full cleardown so the database can be reloaded from scract (Not including ofsted crawl db)
+
+# Remove the sourcerecs collection from the mongo db "localchatter"
+echo Remove localchatter db
+mongo <<!!!
+use localchatter
+db.dropDatabase();
+db.currentRecords.remove({});
+use ofsted_crawl_db
+db.config.drop();
+use lcrecon
+db.sourceRecords.drop();
+!!!
