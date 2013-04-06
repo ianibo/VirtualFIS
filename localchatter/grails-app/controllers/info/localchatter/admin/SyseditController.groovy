e418291db67f68cae20c6759b6f7dfb90270f40f
Ian Ibbotson <ianibbo@gmail.com>
2013-04-06 12:08:33 +0100

    More work on edit subsystem

--- localchatter/grails-app/controllers/info/localchatter/admin/SyseditController.groovy
+++ localchatter/grails-app/controllers/info/localchatter/admin/SyseditController.groovy
@@ -8,6 +8,7 @@ class SyseditController {
 
   def springSecurityService
   def mongoService
+  def mongoTypeService
 
   @Secured(['ROLE_ADMIN', 'IS_AUTHENTICATED_FULLY'])
   def index() { 
@@ -20,6 +21,19 @@ class SyseditController {
     def o = mdb."${params.collection}".findOne(_id:oid)
 
     log.debug("Got object ${o}");
+
+    if ( o ) {
+      def type_info = mongoTypeService.extractTypeInfoFor(o);
+      log.debug("Type info: ${type_info}");
+
+      if (params.type) {
+        log.debug("Get layout for type ${params.type}")
+      }
+      else {
+        // No type specified
+        log.debug("Lookup layout for first type: ${type_info[0].name}");
+      }
+    }
 
     //result.typeInfo = mongoTypeService.identify(o)
     //result.layoutInfo = templateServie.generate
--- localchatter/grails-app/services/localchatter/MongoTypeService.groovy
+++ localchatter/grails-app/services/localchatter/MongoTypeService.groovy
@@ -2,7 +2,30 @@ package localchatter
 
 class MongoTypeService {
 
-    def serviceMethod() {
+  def extractTypeInfoFor(mongo_object) {
 
+    def result = []
+
+    log.debug("Request to extract type info for ${mongo_object._id}");
+    if ( mongo_object.__types ) {
     }
+    else {
+      result.add(reflectBasicTypeInfoFromObject(mongo_object));
+    }
+
+    result
+  }
+
+  def reflectBasicTypeInfoFromObject(mongo_object) {
+    log.debug("No type information present. Generating reflected stub");
+    def result = [:]
+    result.name="__object"
+    def properties = []
+    mongo_object.keySet().each { k ->
+      log.debug("Adding property ${k}");
+      properties.add([name:k,type:k.class.name])
+    }
+    result.properties = properties
+    result;
+  }
 }
