2a8f890d3188157d24e58efb3439797dbc2c2974
Ian Ibbotson <ianibbo@gmail.com>
2013-04-06 11:38:32 +0100

    updated

---
+++ localchatter/grails-app/services/localchatter/MongoTypeService.groovy
@@ -0,0 +1,8 @@
+package localchatter
+
+class MongoTypeService {
+
+    def serviceMethod() {
+
+    }
+}
---
+++ localchatter/test/unit/localchatter/MongoTypeServiceTests.groovy
@@ -0,0 +1,17 @@
+package localchatter
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(MongoTypeService)
+class MongoTypeServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
